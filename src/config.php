313dc467b865816a4da2211d476fbb1254edda2e
Pavel Lashkevych <laspavel@gmail.com>
2018-12-02 15:38:32 +0200

    Added Config DataGen

--- src/config.php
+++ src/config.php
@@ -25,3 +25,16 @@ define("REDIS", [
     'host' => 'myproject-ll-redis',
     'port' => 6379,
 ]);
+
+// Сколько требуется демоданных
+// !! Для заполнения демоданными Categiries > 0 и Authors > 0)
+define("DEMODATA",[
+    'Categories'=>35,
+    'Authors' => 5010,
+    'Articles' => 501000
+]);
+
+
+
+
+
--- src/datagen.php
+++ src/datagen.php
@@ -1,65 +1,65 @@
 <?php
 
-$maxCategories = 35;
-$maxAuthors = 5010;
-$maxArticles = 501000;
-
 require_once 'vendor/autoload.php';
 require_once 'config.php';
 
-$faker = Faker\Factory::create('ru_RU');
-$db = new MysqliDb (DB['host'], DB['username'], DB['password'], DB['dbname']);
-$dbs = new MysqliDb (DBS['host'], DBS['username'], DBS['password'], DBS['dbname']);
+if ((DEMODATA['Authors'] > 0) && (DEMODATA['Categories'] > 0)) {
 
-$countRecord = ($db->rawQueryOne('SELECT COUNT(id) as countrecord FROM `articles` LIMIT 1'))['countrecord'];
-if ($countRecord == 0) {
+    $faker = Faker\Factory::create('ru_RU');
+    $db = new MysqliDb (DB['host'], DB['username'], DB['password'], DB['dbname']);
+    $dbs = new MysqliDb (DBS['host'], DBS['username'], DBS['password'], DBS['dbname']);
 
-    $authors = array();
+    $countRecord = ($db->rawQueryOne('SELECT COUNT(id) as countrecord FROM `articles` LIMIT 1'))['countrecord'];
+    if ($countRecord == 0) {
 
-    for ($i = 1; $i < $maxAuthors; $i++) {
-        $authors[] = array('name' => $faker->firstName . ' ' . $faker->lastName);
+        $authors = array();
+
+        for ($i = 0; $i < DEMODATA['Authors']; $i++) {
+            $authors[] = array('name' => $faker->firstName . ' ' . $faker->lastName);
+        }
+        $maxAuthorId = max($db->insertMulti('authors', $authors));
+        unset($author);
+
+        $category = array();
+        for ($i = 0; $i <= DEMODATA['Categories']; $i++) {
+            $category[] = array('name' => $faker->unique()->word);
+        }
+
+        $maxCategoryId = max($db->insertMulti('categories', $category));
+        unset($category);
+
+        if (DEMODATA['Articles'] > 0) {
+            $articles = array();
+
+            echo "Generating data articles ...\n";
+
+            for ($i = 0; $i <= DEMODATA['Articles']; $i++) {
+                $articles[] = array(
+                    'name' => $faker->sentence,
+                    'article_text' => $faker->paragraph(3, true),
+                    'author_id' => $faker->numberBetween(1, $maxAuthorId),
+                    'category_id' => $faker->numberBetween(1, $maxCategoryId),
+                    'image' => $faker->imageUrl(640, 480, 'cats'),
+                    'viewed' => $faker->numberBetween(1, DEMODATA['Articles']),
+                    'published' => $faker->dateTimeThisYear('now')->format('Y-m-d h:i:s'),
+                );
+            }
+
+            echo "Saving in table ... \n";
+
+            $db->insertMulti('articles', $articles);
+            $articles = array_map(function ($value) {
+                unset($value['article_text']);
+                unset($value['author_id']);
+                unset($value['image']);
+                unset($value['published']);
+                return $value;
+            }, $articles);
+
+            $dbs->insertMulti('articles', $articles);
+        }
     }
-    $maxAuthorId = max($db->insertMulti('authors', $authors));
-    unset($author);
-
-    $category = array();
-    for ($i = 1; $i < $maxCategories; $i++) {
-        $category[] = array('name' => $faker->unique()->word);
-    }
-
-    $maxCategoryId = max($db->insertMulti('categories', $category));
-    unset($category);
-
-    $articles = array();
-
-    echo "Generating data articles ...\n";
-
-    for ($i = 1; $i < $maxArticles; $i++) {
-        $articles[] = array(
-            'name' => $faker->sentence,
-            'article_text' => $faker->paragraph(3, true),
-            'author_id' => $faker->numberBetween(1, $maxAuthorId),
-            'category_id' => $faker->numberBetween(1, $maxCategoryId),
-            'image' => $faker->imageUrl(640, 480, 'cats'),
-            'viewed' => $faker->numberBetween(1, $maxArticles),
-            'published' => $faker->dateTimeThisYear('now')->format('Y-m-d h:i:s'),
-        );
-    }
-
-    echo "Saving in table ... \n";
-
-    $db->insertMulti('articles', $articles);
-    $articles = array_map(function ($value) {
-        unset($value['article_text']);
-        unset($value['author_id']);
-        unset($value['image']);
-        unset($value['published']);
-        return $value;
-    }, $articles);
-
-    $dbs->insertMulti('articles', $articles);
 }
-
 echo "All done.";
 
 ?>
